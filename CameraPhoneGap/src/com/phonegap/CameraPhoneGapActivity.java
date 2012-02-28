c6ddf9d54df6255c08cba709b6fdb3cd4ab2b30b
superiocco <pablo_iocco@hotmail.com>
2012-02-27 21:25:36 -0300

    TODO commit

---
+++ CameraPhoneGap/src/com/phonegap/CameraPhoneGapActivity.java
@@ -0,0 +1,14 @@
+package com.phonegap;
+
+import android.os.Bundle;
+import com.phonegap.*;
+
+public class CameraPhoneGapActivity extends DroidGap
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/main.html");
+    }
+}

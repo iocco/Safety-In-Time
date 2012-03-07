82eec5c0c94f5ae027297122ff410d222ecc6f56
superiocco <pablo_iocco@hotmail.com>
2012-03-07 03:27:27 -0300

    comentarios

---
+++ CameraPhoneGap/assets/www/ayuda.js
@@ -0,0 +1,40 @@
+ 
+
+function ayuda(p){
+    	if(p==1){
+    		
+    		alert("Escribe un Comentario Opcional hasta 100 caracteres sobre el suceso");
+    		
+    	}
+    	
+		if(p==2){
+    		
+    		alert("Selecciona el tipo de evento que deseas reportar, para malos funcionamientos presiona 'Daño'");
+    		
+    	}
+		if(p==3){
+    		
+    		alert("Ingresa tu usuario y password de Facebook");
+    		
+    	}
+    	
+		if(p==4){
+    		
+    		alert("Ingresa tu usuario y password de Twitter");
+    		
+    	}
+		if(p==5){
+    		
+    		alert("Selecciona las Configuracion que quieras realizar");
+    		
+    	}
+		if(p==6){
+    		
+    		alert("Registra tu equipo para ver tu perfil en www.smartpreventions.com");
+    		
+    	}
+		
+		
+    	
+    	
+    }
